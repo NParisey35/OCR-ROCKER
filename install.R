276ad47679706f8b4aaaa01fcf7d85be224fda10
NParisey35 <87025179+NParisey35@users.noreply.github.com>
2024-11-07 13:47:06 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,12 @@
-install.packages("ggplot2")
+install.packages("factoextra")         # for data visualization
+install.packages("FactoMineR")         # for PCA
+install.packages("tidyverse")          # for data manipulation and visualization
+install.packages("dplyr")              # for data manipulation
+install.packages("ggplot2")            # for data visualization
+install.packages("corrplot")           # for correlation test
+install.packages("sf")                 # for spatial data
+install.packages("rnaturalearth")      # for earth maps
+install.packages("rnaturalearthdata")  # for earth dataset
+install.packages("ggspatial")          # for spatial plot
+install.packages("gridExtra")          # for arranging plot
+install.packages("kableExtra")         # for enhanced tables
